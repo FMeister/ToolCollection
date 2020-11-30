ec672bfb489143ab8064b38b5a0f6ee197bbdea1
Fabian <f.meister@tu-bs.de>
2020-11-30 21:48:50 +0100

    Added basic timer functionality

--- ToolCollection/src/app.ts
+++ ToolCollection/src/app.ts
@@ -1,4 +1,68 @@
+interface Todo {
+  description: string;
+  done: boolean;
+}
+interface TimerPreset {
+  displayName: string;
+  duration: number;
+}
+
 export class App {
-  public message = 'Hello World!';
-  public testNumber = 324324;
+  public heading = "Timer";
+  public todos: Todo[] = [];
+  public todoDescription = '';
+
+  public presetTimerList: TimerPreset[] = [
+    { displayName: "Reis", duration: 7 * 60 },
+    { displayName: "FrenchPress1", duration: 5 * 60 },
+    { displayName: "FrenchPress2", duration: 4 * 60 }
+  ];
+
+  public startTime = new Date();
+  public currentTime = new Date();
+  public secoundsOnTimer = 0;
+
+  public inputSecounds;
+  public inputMinutes;
+  public inputHours;
+
+  public inputTargetSecounds = 0;
+  public timerIsRunning = false;
+
+
+
+  startTimer() {
+    this.startTime = new Date();
+    this.timerIsRunning = true;
+
+    this.inputTargetSecounds = 0;
+    if (this.inputHours != null)
+      this.inputTargetSecounds += this.inputHours * 3600;
+    if (this.inputMinutes != null)
+      this.inputTargetSecounds += this.inputMinutes * 60;
+    if (this.inputSecounds != null)
+      this.inputTargetSecounds += this.inputSecounds;
+
+    this.updateTimer();
+  }
+
+  startPresetTimer(preset: TimerPreset) {
+    this.startTimer();
+    this.inputTargetSecounds = preset.duration;
+  }
+
+  stopTimer() {
+    this.timerIsRunning = false;
+  }
+
+  updateTimer() {
+    this.currentTime = new Date();
+    this.secoundsOnTimer = this.inputTargetSecounds - (this.currentTime.getSeconds() - this.startTime.getSeconds());
+    if (!this.timerIsRunning) {
+      return;
+    } else {
+      requestAnimationFrame(this.updateTimer.bind(this));
+    }
+  }
+
 }
